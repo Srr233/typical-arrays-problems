af3209142fcbc4a5045103ca81ffb3b9cf2ecfa3
Srr233 <Sr233@yandex.ru>
2020-09-30 14:31:12 +0300

    feat:add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (!array || !array.length) return 0;
+
+  let min = array[0];
+  const length = array.length;
+  for (let i = 0; i < length/2; i++) {
+    if (array[i] < min) min = array[i];
+    if (array[length - 1 - i] < min) min = array[length - 1 - i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (!array || !array.length) return 0;
+
+  let max = array[0];
+  const length = array.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (array[i] > max) max = array[i];
+    if (array[length - 1 - i] > max) max = array[length - 1 - i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (!array || !array.length) return 0;
+  
+  let result = 0;
+  for (let i of array) {
+    result += i;
+  }
+  return result / array.length;
 }
